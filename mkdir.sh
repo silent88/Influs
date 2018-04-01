a00896b79ffea750d1ca02496d3f6d80d461d2a3
Sven Godo <svengodo@gmail.com>
2018-04-01 12:41:13 +0200

    ADD, MKDIR STRUCTURE

---
+++ mkdir.sh
@@ -0,0 +1,47 @@
+#MKDIR STRUCTURE
+
+mkdir ./cmd 
+mkdir ./setup
+mkdir ./db
+mkdir ./db/qry
+mkdir ./db/dump 
+mkdir ./src
+mkdir ./lib
+mkdir ./lib/sqlite
+mkdir ./io
+mkdir ./io/in
+mkdir ./io/out
+mkdir ./io/archive
+
+
+echo > ./cmd/.nomedia
+echo > ./setup/.nomedia
+echo > ./db/.nomedia
+echo > ./db/qry/.nomedia
+echo > ./db/dump/.nomedia
+echo > ./src/.nomedia
+echo > ./lib/.nomedia
+echo > ./lib/sqlite/.nomedia
+echo > ./io/.nomedia
+echo > ./io/in/.nomedia
+echo > ./io/out/.nomedia
+echo > ./io/archive/.nomedia
+echo > .nomedia
+
+git add ./cmd/.nomedia
+git add ./setup/.nomedia
+git add ./db/.nomedia
+git add ./db/qry/.nomedia
+git add ./db/dump/.nomedia
+git add ./src/.nomedia
+git add ./lib/.nomedia
+git add ./lib/sqlite/.nomedia
+git add ./io/.nomedia
+git add ./io/in/.nomedia
+git add ./io/out/.nomedia
+git add ./io/archive/.nomedia
+git add .nomedia
+
+#git commit -m "ADD, MKDIR STRUCTURE"
+#git commit -m "ADD, MKDIR STRUCTURE" --date="1522579273"
+
